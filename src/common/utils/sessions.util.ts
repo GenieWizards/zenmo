2d4b0383217337240874433751cc7fc5d73c4d42
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2024-11-10 21:00:09 +0530

    feat: generate session function✨

---
+++ src/common/utils/sessions.util.ts
@@ -0,0 +1,8 @@
+import { encodeBase32LowerCaseNoPadding } from "@oslojs/encoding";
+
+export function generateSessionToken(): string {
+  const tokenBytes = new Uint8Array(20);
+  crypto.getRandomValues(tokenBytes);
+  const token = encodeBase32LowerCaseNoPadding(tokenBytes).toLowerCase();
+  return token;
+}
