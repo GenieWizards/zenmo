516fe0315f922af53e48d179e27dabb45036e6d8
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2024-11-10 18:05:26 +0530

    feat: add predefined role values, json content required method✨

--- src/common/enums/index.ts
+++ src/common/enums/index.ts
@@ -1,7 +1,19 @@
-// NOTE: If updating values update in both
-export const AuthRoles = ["user", "admin"] as const;
+// NOTE: Updating the array will auto update the AuthRoles object
+export const authRolesArr = ["user", "admin"] as const;
 
-export enum AuthRole {
-  USER = "user",
-  ADMIN = "admin",
-}
+type AuthRolesTuple = typeof authRolesArr;
+type AuthRolesValues = AuthRolesTuple[number];
+type AuthRolesType = {
+  [K in Uppercase<AuthRolesValues>]: Lowercase<K>;
+};
+
+export const AuthRoles = authRolesArr.reduce(
+  (acc, role) => ({
+    ...acc,
+    [role.toUpperCase()]: role,
+  }),
+  {} as AuthRolesType,
+);
+
+export type UpperCaseAuthRole = keyof typeof AuthRoles;
+export type AuthRole = (typeof AuthRoles)[UpperCaseAuthRole];
