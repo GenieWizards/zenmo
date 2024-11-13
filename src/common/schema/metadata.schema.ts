a5c8c3cbcb8f9ae89c8d378ed0e2a00d36d961eb
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2024-11-13 13:21:34 +0530

    feat: add additional metadata fields✨

--- src/common/schema/metadata.schema.ts
+++ src/common/schema/metadata.schema.ts
@@ -2,8 +2,15 @@ import { z } from "zod";
 
 export const metadataSchema = z.object({
   total: z.number().int().nonnegative().describe("Total number of records"),
-  page: z.number().int().positive().describe("Current page number"),
-  limit: z.number().int().positive().describe("Number of items per page"),
+  page: z.number().int().positive().optional().describe("Current page number"),
+  limit: z
+    .number()
+    .int()
+    .positive()
+    .optional()
+    .describe("Number of items per page"),
+  sortBy: z.string().optional().describe("Field to sort by"),
+  sortOrder: z.enum(["asc", "desc"]).optional().describe("Sort order"),
   totalPages: z.number().int().nonnegative().describe("Total number of pages"),
   hasNextPage: z.boolean().describe("Whether there is a next page"),
   hasPrevPage: z.boolean().describe("Whether there is a previous page"),
@@ -18,3 +25,7 @@ export const metadataSchema = z.object({
 });
 
 export type TMetadata = z.infer<typeof metadataSchema>;
+export type TQueryMetadataSchema = Omit<
+  TMetadata,
+  "total" | "totalPages" | "hasNextPage" | "hasPrevPage" | "count"
+>;
