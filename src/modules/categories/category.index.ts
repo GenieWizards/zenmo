7091cdbf66b262ba1da5f2be9a34e1daf5eeea11
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2024-11-13 12:09:18 +0530

    feat: add type checking to pre-commit hook and update package.json scripts✨

--- src/modules/categories/category.index.ts
+++ src/modules/categories/category.index.ts
@@ -5,7 +5,7 @@ import * as routes from "./category.routes";
 
 export const categoryRouter = createRouter()
   .openapi(routes.getCategoriesRoute, handlers.getCategories)
-  .openapi(routes.getCategoryRoute, handlers.getCategory)
-  .openapi(routes.createCategoryRoute, handlers.createCategory)
-  .openapi(routes.updateCategoryRoute, handlers.updateCategory)
-  .openapi(routes.deleteCategoryRoute, handlers.deleteCategory);
+  // .openapi(routes.getCategoryRoute, handlers.getCategory)
+  .openapi(routes.createCategoryRoute, handlers.createCategory);
+  // .openapi(routes.updateCategoryRoute, handlers.updateCategory)
+  // .openapi(routes.deleteCategoryRoute, handlers.deleteCategory);
