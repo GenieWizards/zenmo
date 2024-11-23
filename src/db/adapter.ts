c27f867393edf9ebd15d38346ce7edaaea718af9
Shivam Vijaywargi <vijaywargishivam@gmail.com>
2024-11-23 15:53:44 +0530

    feat: add script to clear db in test mode✨

--- src/db/adapter.ts
+++ src/db/adapter.ts
@@ -5,7 +5,7 @@ import env from "@/env";
 
 import * as schema from "./schemas";
 
-const queryClient = postgres(env.DATABASE_URL);
+export const queryClient = postgres(env.DATABASE_URL);
 export const db = drizzle(queryClient, {
   casing: "snake_case",
   schema,
